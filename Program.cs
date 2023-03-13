173accdb05b871d9cc04365bca3c51361c4023fd
ИИП ТТ <kab31lab@gmail.com>
2023-03-13 09:15:05 +0200

    задача 1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Задача 1. Заполнить одномерный массив с клавиатуры
+            //шестью целыми числами. Вывести одномерный массив в обратном порядке.
+            Console.Write("Введите чсило элемента ");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++) //в последовательном порядке
+            {
+                Console.Write($"array[{i}]=");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = array.Length - 1; i >= 0; i--) //в обратном порядке
+            {
+                Console.Write($"array[{i}]=");
+                array[i] = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
